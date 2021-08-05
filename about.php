b7888a4b2b9bc62f9b039aa5c23d78d889bc024b
coolboi1564 <79162414+coolboi1564@users.noreply.github.com>
2021-08-05 19:05:29 -0400

    Add files via upload

---
+++ about.php
@@ -0,0 +1,49 @@
+<html lang="en">
+    <head>
+    <style>
+body {
+      background-image: url('cool.jpg');
+      background-repeat: no-repeat;
+      background-position: center center;
+}
+
+.topnav {
+  overflow: hidden;
+  background-color: #f1f1f1;
+}
+
+.topnav a {
+  float: left;
+  display: block;
+  color: black;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+  border-bottom: 3px solid transparent;
+}
+
+.topnav a:hover {
+  border-bottom: 3px white;
+}
+
+.topnav a.active {
+  border-bottom: 3px white;
+}
+
+
+</style>
+        <title>Moe K.!</title>
+        <link rel="shortcut icon" href="OIP.jpg" type="image/x-icon">
+</head>
+<body>
+<div class="topnav">
+  <image src="Moe K..png">
+  <a class="active" href="home.php">Home</a>
+  <a href="about.php">About</a>
+</div>
+    <h1 style = "color:white;text-align:center;">About Moe's YT<h1>
+        <pre>            I'm a controller player trying to succeed my dreams. 
+            I post clips and shorts to entertain you guys and make your day just a little better:).
+            I will soon be posting longer videos and interact with you guys more soon! </pre>
+</html>
---
+++ home.php
@@ -0,0 +1,52 @@
+<html lang="en">
+    <head>
+    <style>
+body {
+      background-image: url('fortnite.jpg');
+      background-repeat: no-repeat;
+      background-position: center center;
+}
+
+.topnav {
+  overflow: hidden;
+  background-color: #f1f1f1;
+}
+
+.topnav a {
+  float: left;
+  display: block;
+  color: black;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+  border-bottom: 3px solid transparent;
+}
+
+.topnav a:hover {
+  border-bottom: 3px white;
+}
+
+.topnav a.active {
+  border-bottom: 3px white;
+}
+
+
+</style>
+        <title>Moe K.!</title>
+        <link rel="shortcut icon" href="OIP.jpg" type="image/x-icon">
+</head>
+<body>
+<div class="topnav">
+  <image src="Moe K..png">
+  <a class="active" href="home.php">Home</a>
+  <a href="about.php">About</a>
+</div>
+    <h1 style = "color:white;text-align: center;">Sub to Moe!<h1>
+        <p style = "font-size:20px;">Pls click on the button, plsssss</p>
+        <a href = "https://www.youtube.com/channel/UCFMjmlzKhN6mr4lScLVkkhA?sub_confirmation=1" target = "_blank"  ><image src=rsz_subscribe-button-youtube.jpg></a>
+        <p>Check out his latest Video!<p>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/Q84SBylD6AE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</body>
+</html>
+
