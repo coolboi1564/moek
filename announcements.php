049b2ebe002b18e8aaae162d90634a73c9dfd08f
coolboi1564 <79162414+coolboi1564@users.noreply.github.com>
2021-08-06 19:14:39 -0400

    Update announcements.php

--- announcements.php
+++ announcements.php
@@ -1,8 +1,37 @@
 <html>
     <head>
+        <style>
+.topnav {
+  overflow: hidden;
+  background-color: #f1f1f1;
+}
+.topnav a {
+  float: left;
+  display: block;
+  color: black;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+  border-bottom: 3px solid transparent;
+}
+.topnav a:hover {
+  border-bottom: 3px white;
+}
+.topnav a.active {
+  border-bottom: 3px white;
+}
+   
+        </style>
         <title>Announcements</title>
 </head>
 <body>
+    <div class="topnav">
+  <image src="Moe K..png">
+  <a class="active" href="index.php">Home</a>
+  <a href="about.php">About</a>
+  <a href="announcements.php">Annnouncements<a>    
+</div>
     <h1>Announcements/What's New!</h1>
     <pre style = "position:center;">     FYI, Moe is gifting a random subscriber at 100
      subs! Add his epic username;Ferrooo76!</pre>  
