6329cba892d2327df706e8d278babb6c765d7775
coolboi1564 <79162414+coolboi1564@users.noreply.github.com>
2021-08-05 21:37:11 -0400

    Update index.php

--- index.php
+++ index.php
@@ -47,10 +47,8 @@ body {
         <a href = "https://www.youtube.com/channel/UCFMjmlzKhN6mr4lScLVkkhA?sub_confirmation=1" target = "_blank"  ><image src=rsz_subscribe-button-youtube.jpg></a>
         <p>Check out his latest Video!<p>
         <iframe width="560" height="315" src="https://www.youtube.com/embed/Q84SBylD6AE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        <pre style = "position:fixed;right:-275px;left:auto;top:150px;">     FYI, Moe is gifting a 
-        random subscriber at 100
-     subs! Add his epic username;
-     Ferrooo76!</pre>  
+        <pre style = "position:fixed;right:-275px;left:auto;top:150px;">     FYI, Moe is gifting a random subscriber at 100
+     subs! Add his epic username;Ferrooo76!</pre>  
                                                                      
 </body>
 </html>
