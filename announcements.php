0612eae40ed4f4e1bf8b1939ed66a786ab5fe900
coolboi1564 <79162414+coolboi1564@users.noreply.github.com>
2021-08-06 19:24:24 -0400

    Update announcements.php

--- announcements.php
+++ announcements.php
@@ -33,7 +33,7 @@
   <a href="announcements.php">Annnouncements<a>    
 </div>
     <h1 style="text-align:center;">Announcements/What's New!</h1>
-    <pre style = "position:center;">     FYI, Moe is gifting a random subscriber at 100
+    <pre style = "text-align:center;">     FYI, Moe is gifting a random subscriber at 100
      subs! Add his epic username;Ferrooo76!</pre>  
 </body>
 </html>
