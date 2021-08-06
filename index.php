5f4d227ae6a3e4b546f75f9ccb9383b63e8c0a58
coolboi1564 <79162414+coolboi1564@users.noreply.github.com>
2021-08-06 19:07:51 -0400

    Update index.php

--- index.php
+++ index.php
@@ -41,16 +41,13 @@ body {
   <image src="Moe K..png">
   <a class="active" href="index.php">Home</a>
   <a href="about.php">About</a>
-  <a href="#announcements">Annnouncements<a>    
+  <a href="announcements.php">Annnouncements<a>    
 </div>
     <h1 style = "color:white;text-align: center;">Sub to Moe!<h1>
         <p style = "font-size:20px;">Pls click on the button, plsssss</p>
         <a href = "https://www.youtube.com/channel/UCFMjmlzKhN6mr4lScLVkkhA?sub_confirmation=1" target = "_blank"  ><image src=rsz_subscribe-button-youtube.jpg></a>
         <p>Check out his latest Video!<p>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/s2fNYuzHs90" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        <pre style = "position:fixed;right:-2px;top:150px;">     FYI, Moe is gifting a random subscriber at 100
-     subs! Add his epic username;Ferrooo76!</pre>  
-                                                                     
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/s2fNYuzHs90" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>                                                              
 </body>
 </html>
 
