92502e659d2c120a72d6ef54f99460f7830d3159
coolboi1564 <79162414+coolboi1564@users.noreply.github.com>
2021-08-06 22:57:59 -0400

    Update nav.php

--- nav.php
+++ nav.php
@@ -6,7 +6,8 @@
   <image src="Moe K..png">
   <a class="active" href="index.php">Home</a>
   <a href="about.php">About</a>
-  <a href="announcements.php">Announcements</a>    
+  <a href="announcements.php">Announcements</a>
+  <a href="suggestions.php">Suggestions</a>
 </div>
 </body>
 </html>
