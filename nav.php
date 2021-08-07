1356bdd0bd9f8ed56156576c58107b6d2f454a56
coolboi1564 <79162414+coolboi1564@users.noreply.github.com>
2021-08-06 22:52:20 -0400

    Add files via upload

---
+++ nav.php
@@ -0,0 +1,12 @@
+
+
+<html>
+  <body>
+<div class="topnav">
+  <image src="Moe K..png">
+  <a class="active" href="index.php">Home</a>
+  <a href="about.php">About</a>
+  <a href="announcements.php">Announcements</a>    
+</div>
+</body>
+</html>
