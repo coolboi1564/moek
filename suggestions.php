784864ea573539946b0ca4d30dd9fed64b2155f9
coolboi1564 <79162414+coolboi1564@users.noreply.github.com>
2021-08-06 23:04:55 -0400

    Update suggestions.php

--- suggestions.php
+++ suggestions.php
@@ -30,15 +30,15 @@
        <link rel= href="OIP.jpg" type ="image/x-icon">          
 </head>
 <body>
-    <h1 style = "text-align:center;">Video Suggestions</h1>
 <form>
     <?php
     require "nav.php"
     ?>
+  <h1 style = "text-align:center;">Video Suggestions!</h1>  
   <label for="sugg">Suggestion:</label>
   <input type="text" id="fname" name="Suggestion" placeholder="Doesn't Work"><br><br>
   <label for="fname">First Name:</label>
-  <input type="text" id="fname" name="First Name"><br><br>
+  <input type="text" id="fname" name="First Name" placeholder="Doesn't Work"><br><br>
   <input type="submit" value="Submit">
 </form>
 </body>
